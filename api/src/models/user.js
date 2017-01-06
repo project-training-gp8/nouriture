e72a873649f5c888e662017c335f976c1cfa93d9
Astheriel <astheriel@gmail.com>
2017-01-06 20:37:49 +0000

    Git is good

--- api/src/models/user.js
+++ api/src/models/user.js
@@ -2,6 +2,8 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 module.exports = mongoose.model("User", new Schema({
+	firstName: String,
+	lastName: String,
 	name: String,
 	email: String,
 	pass: String,//and so much more
